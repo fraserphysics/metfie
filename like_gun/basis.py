08fa3dd184160a421447e597df280120a3e3b3a2
Andrew Fraser <andy@cathcart.local>
2015-11-04 22:22:43 -0700

    "make -j8" works
    Fixed loop due to date on figs dir changing when contents changed.

--- like_gun/basis.py
+++ like_gun/basis.py
@@ -1,6 +1,6 @@
 '''Illustrate spline basis function and its derivatives.
 '''
-from __future__ import print_function
+
 from eos import Spline
 import numpy as np
 def make_figure(plt):
@@ -18,13 +18,9 @@ def make_figure(plt):
     n_x = len(x)
     d = np.empty((n_d,n_x))
     d2 = np.empty((len(t),len(c)))
-    np.set_printoptions(precision=3)
     for k in range(len(c)):
         c[k] = 1.0
         s.new_c(c)
-        if k >= len(c)-2:
-            print("k={0}\n f'[-1]={1}\n f[-1]={2}".format(
-                k, s.derivative(1)(t[-1]), s(t[-1])))
         d2[:,k] = s.derivative(2)(t)
         for i in range(n_x):
             d[:,i] = s.derivatives(x[i])
@@ -35,7 +31,6 @@ def make_figure(plt):
                 ax[i].plot(x,d[i,:], label=str(k))
             ax[i].plot(t,np.zeros(t.shape),'rx')
         c[k] = 0.0
-    print(d2)
     for axis, label in zip(ax, "f \\frac{df}{dx} \\frac{d^2f}{df^2}".split()):
         axis.set_ylabel(r'${0}$'.format(label))
     return fig
